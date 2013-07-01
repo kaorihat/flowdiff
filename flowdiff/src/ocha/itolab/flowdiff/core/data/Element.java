7f9e94b651a644fc44a29c2af95a7bc6c71fd72e
kaorihat <kaorihat.1113@gmail.com>
2013-07-01 22:38:14 +0900

    コメントの訂正

--- flowdiff/src/ocha/itolab/flowdiff/core/data/Element.java
+++ flowdiff/src/ocha/itolab/flowdiff/core/data/Element.java
@@ -1,16 +1,17 @@
 package ocha.itolab.flowdiff.core.data;
 
-import ocha.itolab.flowdiff.core.streamline.*;
+import ocha.itolab.flowdiff.core.streamline.Streamline;
+import ocha.itolab.flowdiff.core.streamline.StreamlineGenerator;
 
 public class Element {
 	public GridPoint gp[] = new GridPoint[8];
 	
 	// 自分とStreamlineの交差判定を行うメソッド
 	boolean intersect(int myId, Streamline sl, int mode) {
-		if (mode == 0){ // 「ちょいむず」だったら
+		if (mode == 0){ // 「かなりむず」だったら
 			return StreamlineGenerator.lastElementId() == myId;
 		}
-		else if (mode == 1) { // 「かなりむず」だったら
+		else if (mode == 1) { // 「ちょいむず」だったら（初期設定）
 			int elementLength = StreamlineGenerator.elementIds.size();
 			int i;
 			for (i = 0; i < elementLength; i++){
